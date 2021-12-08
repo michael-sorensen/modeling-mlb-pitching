cfdc6d3d57e1a501636e5082230361d3de2d7471
Michael Sorensen <Michael.Sorensen.r@gmail.com>
2021-12-07 19:25:07 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,394 @@
+file = "players.json"
+    // ADDING NAMES TO DROPDOWN
+d3.json(file).then(function(data) {
+    data.forEach(d => {
+        var name = d.name;
+        d3.select("#selector").append("option").text(name);
+    });
+});
+prospectFile = "prospects.json"
+d3.json(prospectFile).then(function(data) {
+    data.forEach(d => {
+        var name = d.name;
+        d3.select("#selector2").append("option").text(name);
+    });
+});
+
+
+var colors = ['#0C2340', '#C81D25', '#9CBDBF', 'F18F01'];
+
+// SET DEFAULT PLAYER PLOTS
+function plotting() {
+    d3.json(file).then(function(data) {
+        defaultPlayer = data[0];
+        var pctFastball = defaultPlayer.n_fastball_formatted;
+        var pctOffspeed = defaultPlayer.n_offspeed_formatted;
+        var pctBreaking = defaultPlayer.n_breaking_formatted;
+        var groundballs = defaultPlayer.groundballs_percent;
+        var flyballs = defaultPlayer.flyballs_percent;
+        var linedrives = defaultPlayer.linedrives_percent;
+        var popups = defaultPlayer.popups_percent;
+        var contact = [groundballs, flyballs, linedrives, popups];
+        var pitches = [pctFastball, pctOffspeed, pctBreaking];
+        /*eras = [];
+        fbs = [];
+        data.forEach(function(d) {
+            eras.push(d.p_k_percent);
+            fbs.push(d.p_era);
+            var allData = [{
+                x: fbs,
+                y: eras,
+                type: 'scatter',
+                mode: 'markers'
+            }];
+
+            var layout = { autosize: true };
+            Plotly.newPlot('scatter', allData, layout);
+        });*/
+
+
+        // ADD RESPONSIVE CONFIG FOR PLOTS
+        var config = { responsive: true };
+
+        // PITCHES PIE CHART
+        var pitchesData = [{
+            values: [pitches],
+            labels: ['Fastball', 'Offspeed', 'Breaking'],
+            type: 'pie',
+            marker: {
+                colors: colors
+            },
+            textposition: "inside",
+            textfont: {
+                family: 'Cairo, sans-serif',
+                size: 15,
+                color: 'white',
+            }
+        }];
+
+        var pitchesLayout = {
+            autosize: true
+        };
+
+        Plotly.newPlot('pie', pitchesData, pitchesLayout, config);
+
+        // CONTACT PIE CHART
+        var contactData = [{
+            values: [contact],
+            labels: ['Ground Balls', 'Fly Balls', 'Line Drives', 'Popups'],
+            type: 'pie',
+            marker: {
+                colors: colors
+            }
+        }];
+
+        var contactLayout = { autosize: true };
+
+        Plotly.newPlot('pie2', contactData, contactLayout, config);
+
+        d3.select("#pName").text(`${defaultPlayer.name}`)
+        d3.select("#data").html(`
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">Age</div>
+        <h5 class="card-data">${defaultPlayer.player_age}</h5>
+        </div>
+        </div>
+
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">Games</div>
+        <h5 class="card-data">${defaultPlayer.p_game}</h5>
+        </div>
+        </div>
+        
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">Innings</div>
+        <h5 class="card-data">${defaultPlayer.p_formatted_ip}</h5>
+        </div>
+        </div>
+        
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">ERA</div>
+        <h5 class="card-data">${defaultPlayer.p_era}</h5>
+        </div>
+        </div>
+        
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">Games</div>
+        <h5 class="card-data">${defaultPlayer.p_game}</h5>
+        </div>
+        </div>`);
+
+
+
+    });
+};
+
+plotting();
+
+function updatePlot() {
+    d3.json(file).then(function(data) {
+        var dropDown = d3.select("#selector");
+        var selection = dropDown.property("value");
+        var updatedPlayer = data.find(data => data.name === selection);
+        console.log(updatedPlayer);
+        var fastballUpdate = updatedPlayer.n_fastball_formatted;
+        var offspeedUpdate = updatedPlayer.n_offspeed_formatted;
+        var breakingUpdate = updatedPlayer.n_breaking_formatted;
+
+
+        var groundballsUpdate = updatedPlayer.groundballs_percent;
+        var flyballsUpdate = updatedPlayer.flyballs_percent;
+        var linedrivesUpdate = updatedPlayer.linedrives_percent;
+        var popupsUpdate = updatedPlayer.popups_percent;
+
+        pitchUpdates = [fastballUpdate, offspeedUpdate, breakingUpdate];
+        contactUpdates = [groundballsUpdate, flyballsUpdate, linedrivesUpdate, popupsUpdate];
+
+
+        // RESTYLE PLOTS WITH NEW DATA
+        Plotly.restyle('pie', 'values', [pitchUpdates]);
+        Plotly.restyle('pie2', 'values', [contactUpdates]);
+
+        // UPDATE SIDEBAR SECTION WITH NEW DEMOGRAPHIC DATA
+
+        d3.select("#pName").text(`${updatedPlayer.name}`)
+        d3.select("#data").html(`
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">Age</div>
+        <h5 class="card-data">${updatedPlayer.player_age}</h5>
+        </div>
+        </div>
+
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">Games</div>
+        <h5 class="card-data">${updatedPlayer.p_game}</h5>
+        </div>
+        </div>
+        
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">Innings</div>
+        <h5 class="card-data">${updatedPlayer.p_formatted_ip}</h5>
+        </div>
+        </div>
+        
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">ERA</div>
+        <h5 class="card-data">${updatedPlayer.p_era}</h5>
+        </div>
+        </div>
+        
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">Games</div>
+        <h5 class="card-data">${updatedPlayer.p_game}</h5>
+        </div>
+        </div>`);
+    });
+};
+
+d3.selectAll("#selector").on("change", updatePlot);
+
+
+
+/*---------------------------------------*/
+
+
+
+// SET DEFAULT PROSPECT PLOTS
+function prospectPlotting() {
+    d3.json(prospectFile).then(function(data) {
+        defaultProspectPlayer = data[0];
+        var pctProspectFastball = defaultPlayer.n_fastball_formatted;
+        var pctProspectOffspeed = defaultPlayer.n_offspeed_formatted;
+        var pctProspectBreaking = defaultPlayer.n_breaking_formatted;
+        var prospectGroundballs = defaultPlayer.groundballs_percent;
+        var prospectFlyballs = defaultPlayer.flyballs_percent;
+        var prospectLinedrives = defaultPlayer.linedrives_percent;
+        var prospectPopups = defaultPlayer.popups_percent;
+        var prospectContact = [prospectGroundballs, prospectFlyballs, prospectLinedrives, prospectPopups];
+        var prospectPitches = [pctProspectFastball, pctProspectOffspeed, pctProspectBreaking];
+
+
+        // ADD RESPONSIVE CONFIG FOR PLOTS
+        var config = { responsive: true };
+
+        // PITCHES PIE CHART
+        var prospectPitchesData = [{
+            values: [prospectPitches],
+            labels: ['Fastball', 'Offspeed', 'Breaking'],
+            type: 'pie',
+            marker: {
+                colors: colors
+            },
+            textposition: "inside",
+            textfont: {
+                family: 'Cairo, sans-serif',
+                size: 15,
+                color: 'white',
+            }
+        }];
+
+        var prospectLayout = {
+            autosize: true,
+            showlegend: true,
+            legend: {
+                x: .1,
+                xanchor: 'right',
+                y: .5,
+                font: {
+                    family: 'Cairo, sans-serif',
+                    size: 13
+                },
+                bgcolor: '#F3F7F7',
+                bordercolor: '#0C2340',
+                borderwidth: 2,
+            },
+            margin: {
+                l: 30,
+                r: 30,
+                b: 10,
+                t: 10,
+                pad: 4
+            }
+        };
+
+        Plotly.newPlot('prospectPie', prospectPitchesData, prospectLayout, config);
+
+        // CONTACT PIE CHART
+        var prospectContactData = [{
+            values: [prospectContact],
+            labels: ['Ground Balls', 'Fly Balls', 'Line Drives', 'Popups'],
+            type: 'pie',
+            marker: {
+                colors: colors
+            },
+            textposition: "inside",
+            textfont: {
+                family: 'Cairo, sans-serif',
+                size: 15,
+                color: 'white',
+            }
+        }];
+        Plotly.newPlot('prospectPie2', prospectContactData, prospectLayout, config);
+
+        d3.select("#pProspectName").text(`${defaultProspectPlayer.name}`)
+        d3.select("#prospectData").html(`
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">Age</div>
+        <h5 class="card-data">${defaultProspectPlayer.player_age}</h5>
+        </div>
+        </div>
+
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">Games</div>
+        <h5 class="card-data">${defaultProspectPlayer.p_game}</h5>
+        </div>
+        </div>
+        
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">Innings</div>
+        <h5 class="card-data">${defaultProspectPlayer.p_formatted_ip}</h5>
+        </div>
+        </div>
+        
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">ERA</div>
+        <h5 class="card-data">${defaultProspectPlayer.p_era}</h5>
+        </div>
+        </div>
+        
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">Games</div>
+        <h5 class="card-data">${defaultProspectPlayer.p_game}</h5>
+        </div>
+        </div>
+        `);
+
+
+
+    });
+};
+
+prospectPlotting();
+
+
+function updatePlot2() {
+    d3.json(prospectFile).then(function(data) {
+        var dropDown2 = d3.select("#selector2");
+        var selection = dropDown2.property("value");
+        var updatedPlayer = data.find(data => data.name === selection);
+        console.log(updatedPlayer);
+        var fastballUpdate = updatedPlayer.n_fastball_formatted;
+        var offspeedUpdate = updatedPlayer.n_offspeed_formatted;
+        var breakingUpdate = updatedPlayer.n_breaking_formatted;
+
+
+        var groundballsUpdate = updatedPlayer.groundballs_percent;
+        var flyballsUpdate = updatedPlayer.flyballs_percent;
+        var linedrivesUpdate = updatedPlayer.linedrives_percent;
+        var popupsUpdate = updatedPlayer.popups_percent;
+
+        pitchUpdates = [fastballUpdate, offspeedUpdate, breakingUpdate];
+        contactUpdates = [groundballsUpdate, flyballsUpdate, linedrivesUpdate, popupsUpdate];
+
+
+        // RESTYLE PLOTS WITH NEW DATA
+        Plotly.restyle('prospectPie', 'values', [pitchUpdates]);
+        Plotly.restyle('prospectPie2', 'values', [contactUpdates]);
+
+        // UPDATE SIDEBAR SECTION WITH NEW DEMOGRAPHIC DATA
+
+        d3.select("#pProspectName").text(`${updatedPlayer.name}`)
+        d3.select("#prospectData").html(`
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">Age</div>
+        <h5 class="card-data">${updatedPlayer.player_age}</h5>
+        </div>
+        </div>
+
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">Games</div>
+        <h5 class="card-data">${updatedPlayer.p_game}</h5>
+        </div>
+        </div>
+        
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">Innings</div>
+        <h5 class="card-data">${updatedPlayer.p_formatted_ip}</h5>
+        </div>
+        </div>
+        
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">ERA</div>
+        <h5 class="card-data">${updatedPlayer.p_era}</h5>
+        </div>
+        </div>
+        
+        <div class="row">
+        <div class="card">
+        <div class="card-header-dark">Games</div>
+        <h5 class="card-data">${updatedPlayer.p_game}</h5>
+        </div>
+        </div>`);
+    });
+};
+
+d3.selectAll("#selector2").on("change", updatePlot2);
